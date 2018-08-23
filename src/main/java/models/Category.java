127a60c1d0820246b7a10caf67841faaa882518b
Gemma <gemma.percival@hotmail.co.uk>
2018-08-23 10:39:44 +0100

    annotated category class for hibernate

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,7 +1,17 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "category_name")
     private String categoryName;
 
     public Category(int id, String categoryName) {
