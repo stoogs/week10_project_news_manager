82ce8e03446ece04465293d8f64ab61ba55fdc12
Stuart Black <stuartblack@gmail.com>
2018-08-23 10:32:43 +0100

    created Journalist constructor, getters and setter

--- src/main/java/models/Journalist.java
+++ src/main/java/models/Journalist.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Journalist {
+    private int id;
+    private String journalistName;
+    //TODO Add Articles Array
+
+    public Journalist(String journalistName) {
+        this.journalistName = journalistName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJournalistName() {
+        return journalistName;
+    }
+
+    public void setJournalistName(String journalistName) {
+        this.journalistName = journalistName;
+    }
 }
