b73d237865a2b36ddb36d0bd378a468725d9bb95
Gemma <gemma.percival@hotmail.co.uk>
2018-08-25 12:09:03 +0100

    added get category in controller

--- src/main/java/controllers/CategoryController.java
+++ src/main/java/controllers/CategoryController.java
@@ -1,5 +1,31 @@
 package controllers;
 
-public class CategoryController {
-}
+import db.DBHelper;
+import models.Category;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class CategoryController {
+    public CategoryController()  {
+        this.setupEndpoints();
+    }
+
+    public void setupEndpoints(){
+
+
+        get("/categories", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Category> categoryList = DBHelper.getAll(Category.class);
+            model.put("template", "templates/categories/index.vtl");
+            model.put("categories", categoryList);
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+    }
+}
