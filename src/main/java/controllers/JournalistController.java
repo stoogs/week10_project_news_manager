6ed9b8ed618a3799ebea3ab20d90876050216689
Stuart Black <stuartblack@gmail.com>
2018-08-25 13:05:22 +0100

    Journalist controller Create working

--- src/main/java/controllers/JournalistController.java
+++ src/main/java/controllers/JournalistController.java
@@ -25,32 +25,45 @@ public class JournalistController {
         //staticFileLocation("/public");
 //        ArrayList<Journalist> journalists = new ArrayList<>();
 
-//        get("/journalists/new", (req, res) -> {
-//            HashMap<String, Object> model = new HashMap<>();
-//            model.put("template", "create.vtl");
-//            return new ModelAndView(model, "layout.vtl");
-//        }, new VelocityTemplateEngine());
+        get("/journalists/new", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            List<Journalist> newJournalists = DBHelper.getAll(Journalist.class);
+            model.put("journalists", newJournalists);
+            model.put("template", "templates/journalists/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
         get("/journalists", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             List<Journalist> journalists = DBHelper.getAll(Journalist.class);
-
             model.put("template", "templates/journalists/index.vtl");
             model.put("journalists", journalists);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
-//        post("/journalists", (req, res) -> {
-//            String jounalistName = req.queryParams("journalistName");
-//            String biography = req.queryParams("biography");
-//
-//            Journalist newJournalist = new Journalist(jounalistName, biography );
-//            journalists.add(newJournalist);
-//            HashMap<String, Object> model = new HashMap<>();
-//            model.put("journalists", journalists);
-//            model.put("template", "journalists.vtl");
-//            return new ModelAndView(model, "layout.vtl");
-//        }, velocityTemplateEngine);
+        post ("/journalists", (req, res) -> {
+            String journalistName = req.queryParams("journalistName");
+            String biography = req.queryParams("biography");
+            Journalist addJournalist = new Journalist(journalistName,biography);
+            System.out.println(addJournalist);
+            DBHelper.save(addJournalist);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/journalists/layout.vtl");
+            res.redirect("/journalists");
+            return new ModelAndView(model, "layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        //String flightNumber = req.queryParams("flightNumber");
+        //            String airline = req.queryParams("airline");
+        //            String departureAirport = req.queryParams("departureAirport");
+        //            String arrivalAirport = req.queryParams("arrivalAirport");
+        //
+        //            Flight newFlight = new Flight(flightNumber, airline, departureAirport, arrivalAirport );
+        //            flights.add(newFlight);
+        //            HashMap<String, Object> model = new HashMap<>();
+        //            model.put("flights", flights);
+        //            model.put("template", "flights.vtl");
+        //            return new ModelAndView(model, "layout.vtl");
     }
 }
 
