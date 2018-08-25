50ca935c349846ce6510c871c533f10d002ea099
Gemma <gemma.percival@hotmail.co.uk>
2018-08-25 13:01:02 +0100

    added create new category function

--- src/main/java/controllers/CategoryController.java
+++ src/main/java/controllers/CategoryController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class CategoryController {
     public CategoryController()  {
@@ -18,7 +19,6 @@ public class CategoryController {
 
     public void setupEndpoints(){
 
-
         get("/categories", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             List<Category> categoryList = DBHelper.getAll(Category.class);
@@ -26,6 +26,27 @@ public class CategoryController {
             model.put("categories", categoryList);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get("/categories/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Category> categoryList = DBHelper.getAll(Category.class);
+            model.put("catergories", categoryList);
+            model.put("template", "templates/categories/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/categories", (req, res) -> {
+            String catergoryName = req.queryParams("categoryName");
+            Category addCategory = new Category(catergoryName);
+            System.out.println(addCategory);
+            DBHelper.save(addCategory);
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/categories/layout.vtl");
+            res.redirect("/categories");
+            return new ModelAndView(model, "layout.vtl");
+        }, new VelocityTemplateEngine());
+
+
 
     }
 }
